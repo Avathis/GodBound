158dc1b7d10fd6677069fd0be5946155974382fd
Bluudeen <78979409+Bluudeen@users.noreply.github.com>
2021-08-03 22:15:18 +0200

    s
    s

--- Source/GodBound/BaseClasses/GameplayEffectExecCalculation.cpp
+++ Source/GodBound/BaseClasses/GameplayEffectExecCalculation.cpp
@@ -2,4 +2,39 @@
 
 
 #include "GameplayEffectExecCalculation.h"
+#include "Attribtes/BaseAttributeSet.h"
+#include "AbilitySystemComponent.h"
 
+UGameplayEffectExecCalculation::UGameplayEffectExecCalculation()
+{
+}
+
+static const FMeleeDamageStatics& MeleeDamageStatics()
+{
+	static FMeleeDamageStatics DmgStatics;
+	return DmgStatics;
+}
+
+void UGameplayEffectExecCalculation::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	Super::Execute_Implementation(ExecutionParams, OutExecutionOutput);
+	UAbilitySystemComponent* TargetABSC = ExecutionParams.GetTargetAbilitySystemComponent();
+	UAbilitySystemComponent* SourceABSC = ExecutionParams.GetSourceAbilitySystemComponent();
+
+	AActor* SourceActor = SourceABSC ? SourceABSC->GetAvatarActor() : nullptr;
+	AActor* TargetActor = TargetABSC ? TargetABSC->GetAvatarActor() : nullptr;
+
+	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluateParameters;
+	EvaluateParameters.SourceTags = SourceTags;
+	EvaluateParameters.TargetTags = TargetTags;
+
+	float BaseDamage = 0.f;
+
+	//ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(MeleeDamageStatics().)
+	//DEFINE_ATTRIBUTE_CAPTUREDEF()
+}
--- Source/GodBound/BaseClasses/GameplayEffectExecCalculation.h
+++ Source/GodBound/BaseClasses/GameplayEffectExecCalculation.h
@@ -4,14 +4,33 @@
 
 #include "CoreMinimal.h"
 #include "GameplayEffectExecutionCalculation.h"
+#include "Attribtes/BaseAttributeSet.h"
+
 #include "GameplayEffectExecCalculation.generated.h"
 
 /**
  * 
  */
+struct FMeleeDamageStatics
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Energy);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Stamina);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Health);
+
+	FMeleeDamageStatics& MeleeDamageStatics()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UBaseAttributeSet, Stamina, Source, true);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UBaseAttributeSet, Energy, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UBaseAttributeSet, Health, Target, false);
+	}
+};
+
 UCLASS()
 class GODBOUND_API UGameplayEffectExecCalculation : public UGameplayEffectExecutionCalculation
 {
 	GENERATED_BODY()
-	
+	public:
+	UGameplayEffectExecCalculation();
+
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
 };
