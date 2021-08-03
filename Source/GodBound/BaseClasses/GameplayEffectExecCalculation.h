6a1f167fddc3ff22e71c6788b1875e00637e467c
Bluudeen <78979409+Bluudeen@users.noreply.github.com>
2021-08-03 15:38:25 +0200

    Done 2.0
    Bases

---
+++ Source/GodBound/BaseClasses/GameplayEffectExecCalculation.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "GameplayEffectExecCalculation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GODBOUND_API UGameplayEffectExecCalculation : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+	
+};
