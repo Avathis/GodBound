07fbbbbe175ba4f715b867f1fe10a06297e42aa6
Bluudeen <78979409+Bluudeen@users.noreply.github.com>
2021-08-10 20:47:03 +0200

    Setup for individual heroes

---
+++ Source/GodBound/BaseClasses/Characters/Hephaestus/Hephaestus.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GodBound/BaseClasses/Characters/PlayableCharacterBase.h"
+#include "Hephaestus.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GODBOUND_API AHephaestus : public APlayableCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/GodBound/BaseClasses/Characters/Zeus/Zeus.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GodBound/BaseClasses/Characters/PlayableCharacterBase.h"
+#include "Zeus.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GODBOUND_API AZeus : public APlayableCharacterBase
+{
+	GENERATED_BODY()
+	
+};
