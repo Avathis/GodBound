3d2c0606917ac385245a0849af704ab2c9d97bc0
Bluudeen <78979409+Bluudeen@users.noreply.github.com>
2021-08-30 00:07:29 +0200

    Significant changes

---
+++ Source/GodBound/BaseClasses/Characters/Hephaestus/GB_HephaestusAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "GB_HephaestusAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GODBOUND_API UGB_HephaestusAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
